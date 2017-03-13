4dc2bae98c7f6858a5215255da6d4bfdad7d7ab5
fdodino <fernando.dodino@gmail.com>
2017-03-13 09:48:02 -0300

    Renaming tables uppercase

--- 10_ManejoProyectos_DDL_MySQL.sql
+++ 10_ManejoProyectos_DDL_MySQL.sql
@@ -1,15 +1,15 @@
-CREATE TABLE Impuestos (
+CREATE TABLE IMPUESTOS (
 	Id int AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	Descripcion varchar (150) NULL ,
 	Valor numeric(18, 0) NULL 
 ) ;
 
-CREATE TABLE Proyectos (
+CREATE TABLE PROYECTOS (
 	Id int AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	Descripcion varchar (150) NOT NULL 
 ) ;
 
-CREATE TABLE Tareas (
+CREATE TABLE TAREAS (
 	Id int AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	Complejidad varchar (1)  NOT NULL ,
 	Tiempo int NOT NULL ,
@@ -18,42 +18,42 @@ CREATE TABLE Tareas (
 	Proyecto_Id int NOT NULL 
 ) ;
 
-CREATE TABLE TareasImpuestos (
+CREATE TABLE TAREASIMPUESTOS (
 	Tarea_Id int NOT NULL ,
 	Impuesto_Id int NOT NULL 
 ) ;
 
-ALTER TABLE TareasImpuestos  ADD 
+ALTER TABLE TAREASIMPUESTOS  ADD 
 	CONSTRAINT PK_TareaImpuesto PRIMARY KEY  CLUSTERED 
 	(
 		Tarea_Id,
 		Impuesto_Id
 	)   ;
 
-ALTER TABLE Tareas ADD 
+ALTER TABLE TAREAS ADD 
 	CONSTRAINT FK_Tarea_Proyecto FOREIGN KEY 
 	(
 		Proyecto_Id
-	) REFERENCES Proyectos (
+	) REFERENCES PROYECTOS (
 		Id
 	) ON DELETE CASCADE  ON UPDATE CASCADE , ADD
 	CONSTRAINT FK_Tarea_Tarea FOREIGN KEY 
 	(
 		TareaPadre_Id
-	) REFERENCES Tareas (
+	) REFERENCES TAREAS (
 		Id
 	);
 
-ALTER TABLE TareasImpuestos ADD 
+ALTER TABLE TAREASIMPUESTOS ADD 
 	CONSTRAINT FK_TareaImpuesto_Impuesto FOREIGN KEY 
 	(
 		Impuesto_Id
-	) REFERENCES Impuestos (
+	) REFERENCES IMPUESTOS (
 		Id
 	), ADD
 	CONSTRAINT FK_TareaImpuesto_Tarea FOREIGN KEY 
 	(
 		Tarea_Id
-	) REFERENCES Tareas (
+	) REFERENCES TAREAS (
 		Id
 	);
